5e184fdf74dfca1ea7632c276f3fb8902ec015c6
Michael Becco <e4e6d4d5@gmail.com>
2017-01-05 11:19:03 -0800

    Add GetTagVehicles#new and Form to Test Against

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'static_pages#home'
-
+  resources :geo_tag_vehicles
   get 'test_page/index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
