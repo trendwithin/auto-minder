18565732e1c55e349f94859244e9875ef0cd88d9
Michael Becco <e4e6d4d5@gmail.com>
2017-01-17 18:17:08 -0800

    Add New State Field to Test and Verify

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170118011648) do
+ActiveRecord::Schema.define(version: 20170118015450) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,8 @@ ActiveRecord::Schema.define(version: 20170118011648) do
     t.text     "additional_info"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "state",           null: false
+    t.integer  "user_id",         null: false
   end
 
   create_table "users", force: :cascade do |t|
--- test/features/spec/geo_tag_vehicles/geo_tag_vehicle_new_test.rb
+++ test/features/spec/geo_tag_vehicles/geo_tag_vehicle_new_test.rb
@@ -13,6 +13,7 @@ feature "User Accepts Geolocation" do
     fill_in 'model', with: 'Celica'
     fill_in 'color', with: 'Black'
     fill_in 'license_plate', with: 'ABCXYZ'
+    fill_in 'state', with: 'WA'
     fill_in 'additional_info', with: 'The Test of Success'
     page.must_have_content 'Submit Info'
     click_button 'Submit Info'
@@ -24,6 +25,7 @@ feature "User Accepts Geolocation" do
     assert_equal 'Celica', vehicle.model
     assert_equal 'Black', vehicle.color
     assert_equal 'ABCXYZ', vehicle.license_plate
+    assert_equal 'WA', vehicle.state
     assert_equal 'The Test of Success', vehicle.additional_info
     assert_equal "40.714224", vehicle.latitude
     assert_equal "-73.961452", vehicle.longitude
@@ -35,6 +37,7 @@ feature 'Submit Info Button Is Not Viewable When Required Fields Have Invalid Da
     user_sign_in users(:vic)
     visit new_geo_tag_vehicle_path
     fill_in 'license_plate', with: 'ABCDEFG'
+    fill_in 'state', with: 'WA'
     page.wont_have_content 'Submit Info'
     fill_in 'make', with: 'Toyota'
     page.must_have_content 'Submit Info'
@@ -44,8 +47,19 @@ feature 'Submit Info Button Is Not Viewable When Required Fields Have Invalid Da
     user_sign_in users(:vic)
     visit new_geo_tag_vehicle_path
     fill_in 'make', with: 'Toyota'
+    fill_in 'state', with: 'WA'
     page.wont_have_content 'Submit Info'
     fill_in 'license_plate', with: 'XYZ'
+    page.must_have_content 'Submit Info'
+  end
+
+  scenario 'State Field is Emtpy', :js => true do
+    user_sign_in users(:vic)
+    visit new_geo_tag_vehicle_path
+    fill_in 'make', with: 'Toyota'
+    fill_in 'license_plate', with: 'XYZ'
+    page.wont_have_content 'Submit Info'
+    fill_in 'state', with: 'WA'
     page.must_have_content 'Submit Info'
   end
 
@@ -53,6 +67,7 @@ feature 'Submit Info Button Is Not Viewable When Required Fields Have Invalid Da
     user_sign_in users(:vic)
     visit new_geo_tag_vehicle_path
     fill_in 'make', with: 'Toyota'
+    fill_in 'state', with:'WA'
     fill_in 'license_plate', with: '12345678'
     page.wont_have_content 'Submit Info'
   end
