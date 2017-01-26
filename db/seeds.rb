67a0d49103705f992df29938989254b2f51ad211
Michael Becco <e4e6d4d5@gmail.com>
2017-01-25 16:45:45 -0800

    Add Seed Data and Fixtures

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create!(email: 'seed_user@example.com', password: 'password')
+100.times do |c|
+  u = User.find_by(email: 'seed_user@example.com')
+  license = [*('a'..'z'),*('0'..'9')].shuffle[0,7].join
+  GeoTagVehicle.create!(
+    make: 'Toyota',
+    model: 'Celica',
+    color: 'Black',
+    license_plate: license,
+    latitude: 47.6188363,
+    longitude: -122.321937,
+    state: 'WA',
+    additional_info: 'Lost Car',
+    user: u
+  )
+end
