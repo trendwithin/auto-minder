5ae0a84673e9fb52a97bbc7b680e3762e53f1452
Michael Becco <e4e6d4d5@gmail.com>
2017-01-25 16:46:07 -0800

    test/models/geo_tag_vehicle_test.rb

--- app/models/geo_tag_vehicle.rb
+++ app/models/geo_tag_vehicle.rb
@@ -1,3 +1,8 @@
 class GeoTagVehicle < ApplicationRecord
   belongs_to :user
+
+  validates :license_plate, presence: true, length: { in: 1..7 }
+  validates :latitude, presence: true
+  validates :longitude, presence: true
+  validates :state, presence: true
 end
