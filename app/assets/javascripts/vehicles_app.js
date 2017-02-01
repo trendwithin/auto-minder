b83efbd5444f333a8cdf232c7088d5fa5298c5ce
Michael Becco <e4e6d4d5@gmail.com>
2017-01-31 19:36:39 -0800

    Create Factory for Ajaz- Add Socrata Data Factory to Build Google Map

--- app/assets/javascripts/vehicles_app.js
+++ app/assets/javascripts/vehicles_app.js
@@ -1,5 +1,75 @@
 var app = angular.module('vehicles', ['ngMessages']);
 
+app.factory('socrataData', function($http, $q) {
+  var getStreetParkingSigns =  function(coords) {
+    var url = "https://data.seattle.gov/resource/atig-uucb.json?$where=within_circle(shape,"
+    + coords.latitude + "," + coords.longitude + ", 100)";
+    return $http.get(url).then(function(response) {
+      return response.data;
+    });
+  };
+  return {
+    getStreetParkingSigns: getStreetParkingSigns
+  };
+});
+
+app.factory('vehicleCoordinats', function($q) {
+  var service = {};
+  var formData = {};
+
+  service.getGeolocationCoordinates = function() {
+    var deferred = $q.defer();
+    navigator.geolocation.getCurrentPosition(
+      function(position) { deferred.resolve(position.coords); },
+      function(error) { deferred.resolve(null); }
+    );
+    return deferred.promise;
+  };
+
+  service.setCoords = function(coordsData){
+      // assigns the coordinates to the form data- Return Data for Google Maps API
+      formData.latitude = coordsData.latitude;
+      formData.longitude = coordsData.longitude;
+      return coordsData;
+    };
+  return service;
+});
+
+// TagMyVehiclesController
+app.controller('TagMyVehiclesController', function ($scope, vehicleCoordinats, socrataData) {
+  $scope.test = vehicleCoordinats.getGeolocationCoordinates().then(vehicleCoordinats.setCoords).then(buildMap);
+
+  function buildMap(coords) {
+    var mapOptions = {
+      zoom: 18,
+      center: new google.maps.LatLng(coords.latitude, coords.longitude),
+    }
+
+    $scope.map = new google.maps.Map(document.getElementById('map'), mapOptions);
+    $scope.marker = new google.maps.Marker( {
+      position: {lat: coords.latitude, lng: coords.longitude },
+      map: $scope.map,
+      title: 'Approximate Location of Vehicle.',
+      label: 'Your Vehicle',
+      icon: 'http://maps.google.com/mapfiles/ms/icons/green-dot.png'
+    });
+    socrataData.getStreetParkingSigns(coords).then(function(data) {
+      $scope.signs = data;
+      angular.forEach(data, function(value, key) {
+        $scope.marker = new google.maps.Marker( {
+          position: new google.maps.LatLng(value.shape_lat, value.shape_lng ),
+          map: $scope.map,
+          title: value.categorydescr,
+        });
+      });
+    }).catch(function() {
+      $scope.error = 'Unable to access data.';
+    });
+    $scope.vehicleMapped = true; // show Submit Button when Map Renders
+  }
+});
+
+
 app.controller('VehicleInputCtrl', function ($scope, $q, $http, $window) {
   var longitude, latitude, coords;
 
@@ -48,7 +118,7 @@ app.controller('VehicleInputCtrl', function ($scope, $q, $http, $window) {
       data: $scope.formData,
       headers: { 'Content-Type': 'application/json' }
     }).then(function(data) {
-      // on success redirect 
+      // on success redirect
       $window.location.href = '/geo_tag_vehicles/show';
     }); // need to handle failure
   };
