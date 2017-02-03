0b431bd4d271bd6e56626c5f8e92baf0f39ce91a
Michael Becco <e4e6d4d5@gmail.com>
2017-02-02 16:37:37 -0800

    Remove Unused Resource Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'static_pages#home'
-  resources :geo_tag_vehicles
-  resources :tag_my_vehicles
+  resources :geo_tag_vehicles, only: [:index, :new, :show, :create]
+  resources :tag_my_vehicles, only: [:new, :create, :show]
   get 'test_page/index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
